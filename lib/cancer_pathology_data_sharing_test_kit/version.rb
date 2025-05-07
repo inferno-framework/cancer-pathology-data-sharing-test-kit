75559f2fb6871c7ec183167716dcf3e74545e5d4
rpassas <rpassas@mitre.org>
2025-05-07 11:24:20 -0400

    release 0.9.0

--- lib/cancer_pathology_data_sharing_test_kit/version.rb
+++ lib/cancer_pathology_data_sharing_test_kit/version.rb
@@ -1,4 +1,4 @@
 module CancerPathologyDataSharingTestKit
-  VERSION = '0.1.0'.freeze
-  LAST_UPDATED = '2025-03-21'.freeze # TODO: update next release
+  VERSION = '0.9.0'.freeze
+  LAST_UPDATED = '2025-05-07'.freeze # TODO: update next release
 end
